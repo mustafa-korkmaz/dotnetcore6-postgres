016c6bafcb74bbb49dfd9400f93682e2ccf83a1e
Mustafa Korkmaz <m.korkmaz@outlook.com>
2022-04-20 23:33:28 +0300

    modify db conn str

--- src/Infrastructure/Persistance/Postgres/DesignTimeDbContextFactory.cs
+++ src/Infrastructure/Persistance/Postgres/DesignTimeDbContextFactory.cs
@@ -12,7 +12,7 @@ namespace Shipper.Api.Dal.Databases.Postgres
         public PostgresDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<PostgresDbContext>();
-            optionsBuilder.UseNpgsql("Server=tai.db.elephantsql.com;Port=5432;Database=ocziorke;User Id=ocziorke;Password=t67v-NthY60rSJPxna--jB2iPE_WQPF8;CommandTimeout=20;");
+            optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres;Password=1907;CommandTimeout=20;");
 
             return new PostgresDbContext(optionsBuilder.Options);
         }
